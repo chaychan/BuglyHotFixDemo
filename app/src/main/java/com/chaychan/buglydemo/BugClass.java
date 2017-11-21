572b6d89dc6e56c77c4d4d2a2638350ff440d8a3
chaychan <844738237@qq.com>
2017-11-21 12:07:57 +0800

    提交demo源码

---
+++ app/src/main/java/com/chaychan/buglydemo/BugClass.java
@@ -0,0 +1,16 @@
+package com.chaychan.buglydemo;
+
+/**
+ * @author ChayChan
+ * @description: TODO
+ * @date 2017/11/17  21:52
+ */
+
+public class BugClass {
+
+    public static String getString(){
+        String str = "Bug had fixed!";
+        int length = str.length();
+        return str;
+    }
+}
